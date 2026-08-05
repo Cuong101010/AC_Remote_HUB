a60d3163fb07afda057258489474826f95f54116
cuong101010 <hanoi19691969@gmail.com>
2026-08-05 16:38:41 +0700

    Revert to stable v0.2 HTTP Polling architecture; remove MQTT and Vercel files

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-backend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend_mqtt"))
-if backend_dir not in sys.path:
-    sys.path.insert(0, backend_dir)
-
-from app_mqtt import app
--- wsgi_mqtt.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend_mqtt'))
-
-from app_mqtt import app as application
-
-if __name__ == '__main__':
-    application.run()
